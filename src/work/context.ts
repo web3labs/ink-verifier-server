a2f8022b132fe402b45978eb931a1c30e66f57a2
Marc Fornos <frozen.pomelo@gmail.com>
2022-10-13 13:10:40 +0200

    extract sub-process management context

---
+++ src/work/context.ts
@@ -0,0 +1,52 @@
+import { ChildProcess } from 'node:child_process'
+
+export class WorkContext {
+  processes: Array<ChildProcess>
+
+  constructor () {
+    this.processes = []
+  }
+
+  addProc (p: ChildProcess) {
+    console.log('adding', p.pid)
+    this.processes.push(p)
+  }
+
+  rmProc (p: ChildProcess) {
+    const i = this.processes.indexOf(p)
+    if (i > -1) {
+      this.processes.splice(i, 1)
+    }
+  }
+
+  get runningProcs () {
+    return this.processes.length
+  }
+
+  onExit (event: string, exitCode: number) {
+    if (this.runningProcs > 0) {
+      process.stdout.write(`${this.runningProcs} sub-process/es running\n`)
+
+      this.processes.forEach(p => {
+        process.stdout.write(`> SIGTERM (${p.pid}): `)
+        if (p.kill()) {
+          process.stdout.write('OK\n')
+        } else {
+          process.stdout.write('FAIL\n')
+          process.stdout.write(`> SIGKILL ${p.pid}: `)
+          if (p.kill(9)) {
+            process.stdout.write('OK\n')
+          } else {
+            process.stdout.write('FAIL\n')
+          }
+        }
+      })
+    }
+    process.exit(exitCode)
+  }
+}
+
+const workContext = new WorkContext()
+
+// Singleton leveraging caching
+export default workContext
--- src/work/docker.ts
+++ src/work/docker.ts
@@ -1,16 +1,9 @@
-import util from 'util'
-import { exec, spawn } from 'node:child_process'
-import { cliTable2Json } from 'cli-table-2-json'
+import { spawn } from 'node:child_process'
 import fs from 'node:fs'
 import path from 'node:path'
 import { FastifyBaseLogger } from 'fastify'
 import { CACHES_DIR, MAX_CONTAINERS, VERIFIER_IMAGE } from '../config'
-
-const pexec = util.promisify(exec)
-
-function splitLines (input: string): string[] {
-  return input.replace(/\r/g, '').split('\n')
-}
+import workContext, { WorkContext } from './context'
 
 interface RunOptions {
   processingDir: string,
@@ -20,20 +13,20 @@ interface RunOptions {
 
 class Docker {
   log: FastifyBaseLogger
+  context: WorkContext
 
   constructor ({ log }: {log: FastifyBaseLogger}) {
     this.log = log
+    this.context = workContext
   }
 
   /**
-   * TBD
+   * Runs a verifier docker container.
    */
   run ({ processingDir, successHandler, errorHandler }: RunOptions) {
-    // TODO empty errors
     const params = [
       'run',
       '--rm',
-      // '--user', '1000:1000', // using this to bypass permission issues T_T
       '--cidfile', path.resolve(processingDir, 'cid'),
       '--security-opt=no-new-privileges',
       '-v', `${processingDir}:/build`,
@@ -46,18 +39,21 @@ class Docker {
     const out = fs.openSync(path.resolve(processingDir, 'out.log'), 'a')
     const err = fs.openSync(path.resolve(processingDir, 'out.log'), 'a')
 
+    // Note that parent will wait the sub-process to finish
     const p = spawn('docker', params, {
       detached: true,
       stdio: ['ignore', out, err]
     })
 
-    this.log.info(`Running verification: pid=${p.pid}`)
+    this.log.info(`Running verification (${p.pid})`)
+
+    this.context.addProc(p)
 
     p.on('close', (code) => {
-      // TODO: Move from processing to if OK verfieds/ in NOK errors/
-      // double check pristine before moving, downloading it again, just in case
-      // compromised and ring alarms if not ??
-      this.log.info(`child process exited with code ${code}`)
+      this.log.info(`${p.pid} exit ${code}`)
+
+      this.context.rmProc(p)
+
       if (code === 0) {
         successHandler()
       } else {
@@ -66,15 +62,8 @@ class Docker {
     })
   }
 
-  async ps () {
-    const { stdout } = await pexec('docker ps')
-    const lines = splitLines(stdout)
-    return cliTable2Json(lines)
-  }
-
   async canRunMore () {
-    const list = await this.ps()
-    return list.length <= MAX_CONTAINERS
+    return this.context.runningProcs <= MAX_CONTAINERS
   }
 }
 
