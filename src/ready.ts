529a1eb4b2b636e5fd2794319659be8bc8e5492e
Marc Fornos <frozen.pomelo@gmail.com>
2022-10-13 13:11:11 +0200

    kill sub-processes on exit

--- src/ready.ts
+++ src/ready.ts
@@ -4,6 +4,7 @@ import { exec } from 'node:child_process'
 
 import { BASE_DIR, DOCKER_ROOTLESS } from './config'
 import { FastifyInstance } from 'fastify'
+import workContext from './work/context'
 
 async function rootless (server: FastifyInstance) {
   // TODO: consider if this belongs here or better to move it out
@@ -32,6 +33,14 @@ function onReady (server: FastifyInstance) {
   if (DOCKER_ROOTLESS) {
     rootless(server)
   }
+
+  const EXIT_SIGNALS = [
+    'SIGINT',
+    'SIGTERM'
+  ]
+  EXIT_SIGNALS.forEach(eventName => {
+    process.on(eventName, workContext.onExit.bind(workContext))
+  })
 }
 
 export default onReady
