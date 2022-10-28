3842ea9c6ea58a03fc97f573cedf6c8737050da2
Marc Fornos <frozen.pomelo@gmail.com>
2022-10-28 12:09:57 +0200

    add PM2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: 'ink-verification-service',
+    script: './build/start.js',
+    env_production: {
+      NODE_ENV: 'production',
+      SERVER_HOST: '0.0.0.0',
+      BASE_DIR: '/opt/verifier/data',
+      CACHES_DIR: '/opt/verifier/caches',
+      PUBLISH_DIR: '/opt/verifier/publish',
+      VERIFIER_IMAGE: 'ink-verifier:latest',
+      CONTAINER_ENGINE: 'podman'
+    },
+    env_development: {
+      NODE_ENV: 'development'
+    }
+  }]
+}
