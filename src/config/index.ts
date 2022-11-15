c220a3e4c08e753cc7885470981c946441b57318
Marc Fornos <frozen.pomelo@gmail.com>
2022-11-15 14:43:20 +0100

    change default port

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,7 +10,7 @@ export const MAX_CONTAINERS = Number(process.env.MAX_CONTAINERS || 5)
 export const VERIFIER_IMAGE = process.env.VERIFIER_IMAGE || 'ink-verifier:develop'
 export const CACHES_DIR = process.env.CACHES_DIR || path.resolve(__dirname, '../../.tmp/caches')
 export const SERVER_HOST = process.env.SERVER_HOST || '127.0.0.1'
-export const SERVER_PORT = Number(process.env.SERVER_PORT || 3000)
+export const SERVER_PORT = Number(process.env.SERVER_PORT || 3001)
 export const OAS_URL = process.env.OAS_URL || `http://${SERVER_HOST}:${SERVER_PORT}`
 export const CONTAINER_ENGINE = process.env.CONTAINER_ENGINE || 'docker'
 // e.g. '--user uid:gid'
